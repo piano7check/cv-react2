975f010b952fdb76ceba2a917efd1c29c320bc27
Gustavo Rojas <115680051+piano7check@users.noreply.github.com>
2026-02-20 01:49:36 +0000

    commint inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hola Mundo</div>
+    </div>
+  );
+}
+
+export default App;
